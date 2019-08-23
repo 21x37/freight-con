066f6e3315c61a6968fdbce3ff25308cc1fc07a3
Tyler Mok <tylermok@outlook.com>
2019-08-22 20:56:08 -0700

    add photos

--- src/components/home/ImageCarousel.js
+++ src/components/home/ImageCarousel.js
@@ -7,7 +7,10 @@ class ImageCarsouel extends React.Component {
         this.state = {
             imageSrcPath: [
                 'https://images.pexels.com/photos/93398/pexels-photo-93398.jpeg',
-                'https://images.pexels.com/photos/192364/pexels-photo-192364.jpeg'
+                'https://images.pexels.com/photos/192364/pexels-photo-192364.jpeg',
+                'https://images.unsplash.com/photo-1562811950-41d4a4944a4b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80',
+                'https://images.unsplash.com/photo-1492168732976-2676c584c675?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1100&q=80',
+                'https://images.unsplash.com/photo-1541969487406-1f1adf3884ab?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1655&q=80'
             ],
             index: 0
         }
